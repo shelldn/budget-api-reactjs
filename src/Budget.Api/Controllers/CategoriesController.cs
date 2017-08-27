c9c705eedb005b1be5619cb5f17d77c6a7f61634
Yevgeny Shirin <yevgeny.shirin@gmail.com>
2017-08-27 20:21:31 +0300

    Api project init, /api/categories controller.

---
+++ src/Budget.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Budget.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public sealed class CategoriesController
+    {
+        [HttpGet]
+        public IEnumerable<string> GetAll()
+        {
+            return new[] { "Salary", "Food" };
+        }
+    }
+}
