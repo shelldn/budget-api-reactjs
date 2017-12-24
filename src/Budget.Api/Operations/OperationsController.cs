35728eb48404bcfa4d5613a4cd3c614a2de0aa3d
Yevgeny Shirin <yevgenys@varyence.com>
2017-12-24 17:09:29 +0200

    OperationsController.

---
+++ src/Budget.Api/Operations/OperationsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Budget.Api.Operations
+{
+    [Route("api/[controller]")]
+    public sealed class OperationsController : Controller
+    {
+        [HttpPost("/api/budgets/{year:int}/[controller]")]
+        public IActionResult Create(int year, [FromBody] OperationSummary operation)
+        {
+            var client = new MongoClient("mongodb://192.168.255.129:27017");
+            var db = client.GetDatabase("budgetio");
+            var operations = db.GetCollection<BsonDocument>("operations");
+
+            var doc = new BsonDocument
+            {
+                ["budget_id"] = year,
+                ["category_id"] = operation.CategoryId,
+                ["month"] = operation.Month,
+                ["plan"] = operation.Plan,
+                ["fact"] = operation.Fact
+            };
+
+            operations.InsertOne(doc);
+
+            operation.Id = doc["_id"].AsObjectId.ToString();
+
+            return Created("", operation);
+        }
+    }
+}
